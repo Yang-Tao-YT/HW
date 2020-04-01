ee5b9ef8ab3c002405a53ccf767fc1b05f680cff
yang-tao-1 <57275485+yang-tao-1@users.noreply.github.com>
2020-03-31 23:14:22 -0400

    update

--- 829/HW6(unfinished).py
+++ 829/HW6(unfinished).py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[28]:
+# In[40]:
 
 
 from scipy.stats import norm
@@ -12,12 +12,18 @@ t = 0
 s = 1200
 k = 1200
 r = np.log(100/98.45) /3 * 12
-
-def bs(s,k,r,T,t,sigma):
-    d1 = 1/(sigma * (T - t)**0.5) * (np.log(s/k) - (r + sigma**2 /2)*(T - t))
+q = 2 /100
+def bs(s,k,r,T,t,sigma,q):
+    d1 = 1/(sigma * (T - t)**0.5) * (np.log(s/k) + (r - q + sigma**2 /2)*(T - t))
     d2 = d1 - (sigma * (T - t)**0.5)
-    return norm.cdf(d1) * s - norm.cdf(d2) * k * np.exp(r * (T - t))
-bs(s,k,r,T,t,sigma)
+    return -norm.cdf(-d1) * s + norm.cdf(-d2) * k * np.exp(-r * (T - t))
+bs(s,k,r,T,t,sigma,q)
+
+
+# In[ ]:
+
+
+
 
 
 # In[ ]:
